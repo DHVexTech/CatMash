b22e3ade28d892bacab92bfb82486d0594dbfde8
VexTech <herve@intechinfo.fr>
2017-11-20 20:57:02 +0100

    Get the JSON of cats & add the pictures of cats in Website

--- CatMash/CatMash/Controllers/HomeController.cs
+++ CatMash/CatMash/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CatMash.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace CatMash.Controllers
 {
     public class HomeController : Controller
     {
+        public CatContext catContext = new CatContext();
+
         public ActionResult Index()
         {
             return View();
@@ -16,8 +19,8 @@ namespace CatMash.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
+            List<Cat> cats = catContext.GetAll();
+            return View(cats);
         }
 
         public ActionResult Contact()
---
+++ CatMash/CatMash/Models/CatContext.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace CatMash.Models
+{
+    public class Cat
+    {
+        public string ID { get; set; }
+        public string URL { get; set; }
+    }
+
+    public class CatContext
+    {
+        public List<Cat> GetAll()
+        {
+            var json = new WebClient().DownloadString("https://latelier.co/data/cats.json");
+            JObject catsJson = JObject.Parse(json);
+            List<JToken> results = catsJson["images"].Children().ToList();
+            List<Cat> cats = new List<Cat>();
+            foreach (JToken result in results)
+            {
+                Cat cat = result.ToObject<Cat>();
+
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(cat.URL);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                        cats.Add(cat);
+                }
+                catch(Exception e) {}
+            }
+            return cats;
+        }
+    }
+}
